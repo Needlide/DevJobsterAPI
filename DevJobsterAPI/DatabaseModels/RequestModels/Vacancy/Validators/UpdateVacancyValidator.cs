c23ca93f8344fddacef345bb7e55249b7052c765
Needlide <max.litvinyuck553@gmail.com>
2025-04-16 00:06:28 +0300

    chore: add update vacancy validator

---
+++ DevJobsterAPI/DatabaseModels/RequestModels/Vacancy/Validators/UpdateVacancyValidator.cs
@@ -0,0 +1,39 @@
+using FluentValidation;
+
+namespace DevJobsterAPI.DatabaseModels.RequestModels.Vacancy.Validators;
+
+public class UpdateVacancyValidator : AbstractValidator<UpdateVacancy>
+{
+    public UpdateVacancyValidator()
+    {
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .Length(30, 700).WithMessage("Description must be between 30 and 700 characters");
+        RuleFor(x => x.Salary)
+            .NotEmpty().WithMessage("Salary is required")
+            .GreaterThan(0).WithMessage("Salary must be greater than 0");
+        RuleFor(x => x.Requirements)
+            .NotEmpty().WithMessage("Requirements cannot be empty")
+            .Length(30, 250).WithMessage("Requirements must be between 30 and 250 characters");
+        RuleFor(x => x.CompanyWebsite)
+            .NotEmpty().WithMessage("Company's website link is required")
+            .MaximumLength(250).WithMessage("Company's website link must be less than 250 characters")
+            .Must(BeAValidUrl).WithMessage("Company's website link must be a valid URL");
+        RuleFor(x => x.TypeOfJob)
+            .NotEmpty()
+            .Length(1);
+        RuleFor(x => x.Location)
+            .NotEmpty()
+            .Length(1);
+        RuleFor(x => x.Benefits)
+            .Length(15, 250).WithMessage("Benefits must be between 15 and 250 characters");
+    }
+
+    private static bool BeAValidUrl(string? url)
+    {
+        if (string.IsNullOrEmpty(url)) return false;
+
+        return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+               && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+    }
+}
