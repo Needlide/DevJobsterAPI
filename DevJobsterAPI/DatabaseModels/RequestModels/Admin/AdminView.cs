15f470756cf33c686ab00f32c76e626efedae584
Needlide <max.litvinyuck553@gmail.com>
2025-04-17 00:12:11 +0300

    chore: remove unused dependency

--- DevJobsterAPI/DatabaseModels/RequestModels/Admin/AdminView.cs
+++ DevJobsterAPI/DatabaseModels/RequestModels/Admin/AdminView.cs
@@ -1,5 +1,3 @@
-using DevJobsterAPI.DatabaseModels.Security;
-
 namespace DevJobsterAPI.DatabaseModels.RequestModels.Admin;
 
 public record AdminView(string FirstName, string LastName, bool Role); // true - moderator, false - admin
