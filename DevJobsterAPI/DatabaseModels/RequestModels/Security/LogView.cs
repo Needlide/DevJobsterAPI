d37b43b92845fa09bc9d4feab0162ee87cb07aa3
Needlide <max.litvinyuck553@gmail.com>
2025-04-17 00:11:10 +0300

    refactor: allow nullable Admin property

--- DevJobsterAPI/DatabaseModels/RequestModels/Security/LogView.cs
+++ DevJobsterAPI/DatabaseModels/RequestModels/Security/LogView.cs
@@ -1,3 +1,3 @@
 namespace DevJobsterAPI.DatabaseModels.RequestModels.Security;
 
-public record LogView(string Body, DatabaseModels.Admin.Admin Admin, DateTime CreatedAt);
+public record LogView(string Body, DatabaseModels.Admin.Admin? Admin, DateTime CreatedAt);
